e0f8ac4acd148ff8db31d11cfc0a7341120001c0
Bean <lijiang123meng@163.com>
2017-06-10 20:21:22 +0800

    2048-4

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 /**
  * Created by Lenovo on 2017/6/10.
  */
-var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0xBC8F8F});
+var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0xCCCCCC});
 document.body.appendChild(app.view);
+var maxCount=16;
+var currentCount=0;
+var score=0;
+
 var basicText = new PIXI.Text('2048 ', {fontSize: 60});
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width / 2;
-basicText.y = app.renderer.width / 2;
+basicText.y = app.renderer.height / 4;
 app.stage.addChild(basicText);
+
+
+var scoreText = new PIXI.Text('Score: '+score, {fontSize: 28});
+scoreText.anchor.set(0.5);
+scoreText.x = app.renderer.width / 2;
+scoreText.y = app.renderer.height/ 10*8;
+app.stage.addChild(scoreText);
 
 
 var grid = [];
@@ -21,7 +32,8 @@ function flushUI() {
         }
 
     }
-}
+    scoreText.text='Score:'+score;
+};
 flushUI();
 
 function oppearNumber() {
@@ -29,13 +41,10 @@ function oppearNumber() {
 }
 
 function drawcell(xIndex, yIndex) {
-    var color = 0XFFF0F5;
-    if (grid[xIndex][yIndex] === 2) {
-        color = 0xFF0000;
-    }
+
 
     var graphics = new PIXI.Graphics();
-    graphics.beginFill(color, 1);
+    graphics.beginFill(getColorByNumber(grid[xIndex][yIndex]), 1);
     graphics.drawRect(app.renderer.width / 4 + yIndex * 51, app.renderer.height / 8 * 3 + xIndex * 51, 50, 50);
     app.stage.addChild(graphics);
     if (grid[xIndex][yIndex] != 0) {
@@ -48,19 +57,123 @@ function drawcell(xIndex, yIndex) {
 
 
 }
-var xIndex = oppearNumber();
-var yIndex = oppearNumber();
-grid[xIndex][yIndex] = 2;
-drawcell(xIndex, yIndex);
+function getColorByNumber(number) {
+    var colorValue = {
+        0: 0x00FF00,
+        2: 0xFF0000,
+        4: 0x0000FF,
+    };
+    var color = colorValue[number];
+    if (color === undefined) {
+        color = 0xff0ff;
+    }
+    return color;
+}
+function addRandomCell() {
+    if(currentCount===maxCount)return ;
+    var xIndex = oppearNumber();
+    var yIndex = oppearNumber();
+    while (grid[xIndex][yIndex] !== 0) {
+        var xIndex = oppearNumber();
+        var yIndex = oppearNumber();
+    }
+    grid[xIndex][yIndex] = 2;
+    currentCount++;
 
+}
+addRandomCell();
+addRandomCell();
+flushUI();
+var onToDownEventHandler = function () {
+    rotateArray(3);
+    var isChanged = moveCellToRight();
+    rotateArray(1);
+    if (isChanged) {
+        addRandomCell();
+    }
+    flushUI();
+    if (checkGameOver()) {
+        alert('Game over.');
+    }
+};
+var onToLeftEventHandler = function () {
+    rotateArray(2);
+    var isChange = moveCellToRight();
+    rotateArray(2);
+    if (isChange) {
+        addRandomCell();
+    }
+    flushUI();
+    if (checkGameOver()) {
+        alert('Game over.');
+    }
+};
+var onToUpEventHandler = function () {
+    rotateArray(1);
+    var isChange = moveCellToRight();
+    rotateArray(3);
+    if (isChange) {
+        addRandomCell();
+    }
+    flushUI();
+    if (checkGameOver()) {
+        alert('Game over.');
+    }
+};
 document.addEventListener('keydown', function (event) {
+    var isChange;
     if (event.key === 'ArrowRight') {
-        moveCellToRight();
+        isChange=moveCellToRight();
+        if(isChange){
+            addRandomCell();
+        }
+
+        flushUI();
+        if(checkGameOver()){
+            alter('Game Over');
+        }
+    };
+    if (event.key === 'ArrowUp') {
+        rotateArray(1);
+        isChange=moveCellToRight();
+        rotateArray(3);
+        if(isChange){
+            addRandomCell();
+        }
+        ;
+        flushUI();
+        if(checkGameOver()){
+            alter('Game Over');}
+    }
+    if (event.key === 'ArrowLeft') {
+        rotateArray(2);
+        isChange=moveCellToRight();
+        rotateArray(2);
+        if(isChange)
+        {
+            addRandomCell();
+        }
+        ;
+        flushUI();
+        if(checkGameOver()){
+            alter('Game Over');}
+    }
+
+    if (event.key === 'ArrowDown') {
+        rotateArray(3);
+        isChange=moveCellToRight();
+        rotateArray(1);
+        if(isChange){
+            addRandomCell();
+        }
         flushUI();
     }
+    if(checkGameOver()){
+        alter('Game Over');}
 
 });
 function moveCellToRight() {
+    var isChange=false;
     for (var xIndex = 0; xIndex < 4; xIndex++) {
         for (var yIndex = 2; yIndex >= 0; yIndex--) {
             if (grid[xIndex][yIndex] === 0) continue;
@@ -68,14 +181,20 @@ function moveCellToRight() {
             if (theEmptyCellIndex !== -1) {
                 grid[xIndex][theEmptyCellIndex] = grid[xIndex][yIndex];
                 grid[xIndex][yIndex] = 0;
+            isChange=true;
+            }
 
-                if (grid[xIndex][theEmptyCellIndex] === grid[xIndex][theEmptyCellIndex + 1]) {
-                    grid[xIndex][theEmptyCellIndex + 1] += grid[xIndex][theEmptyCellIndex];
-                    grid[xIndex][theEmptyCellIndex] = 0;
-                }
+            var currentIndex = theEmptyCellIndex === -1 ? yIndex : theEmptyCellIndex;
+            if (grid[xIndex][currentIndex] === grid[xIndex][currentIndex + 1]) {
+                grid[xIndex][currentIndex + 1] += grid[xIndex][currentIndex];
+                grid[xIndex][currentIndex] = 0;
+                score+=grid[xIndex][currentIndex+1];
+                isChange=true;
+                currentCount--;
             }
         }
     }
+    return isChange;
 }
 
 
@@ -87,4 +206,34 @@ function findTheFirstRightCell(xIndex, yIndex) {
     }
 
     return -1;
+}
+function rotateArray(rotateCount = 1) {
+    for (var i = 0; i < rotateCount; i++) {
+        grid = rotateArrayToRightOnce(grid);
+    }
+
+    function rotateArrayToRightOnce(array) {
+        return array.map((row, xIndex) => {
+            return row.map((item, yIndex) => {
+                return array[3 - yIndex][xIndex];
+            })
+        })
+    }
+}
+function checkGameOver() {
+    if (currentCount !== maxCount) return false;
+
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (grid[i][j] === grid[i][j - 1] ||
+                grid[i][j] === grid[i][j + 1] ||
+                (grid[i-1] && grid[i][j] === grid[i - 1][j]) ||
+                (grid[i+1] && grid[i][j] === grid[i + 1][j])
+            ) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
